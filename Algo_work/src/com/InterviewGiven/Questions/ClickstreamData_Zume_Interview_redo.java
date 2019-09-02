ce43b7b57e221c966bf09d57b77362ec9dc43788
rajataggarwal1 <rajataggarwal2@gmail.com>
2019-09-01 22:03:41 -0700

    Complete Zume redo medium question

--- Algo_work/src/com/InterviewGiven/Questions/ClickstreamData_Zume_Interview_redo.java
+++ Algo_work/src/com/InterviewGiven/Questions/ClickstreamData_Zume_Interview_redo.java
@@ -1,5 +1,8 @@
 package com.InterviewGiven.Questions;
 
+import java.util.ArrayList;
+import java.util.List;
+
 /*
  * // We have some clickstream data that we gathered on our client's website. Using cookies, we collected snippets of users' anonymized URL histories while they browsed the site. The histories are in chronological order and no URL was visited more than once per person.
 
@@ -65,13 +68,98 @@ public class ClickstreamData_Zume_Interview_redo {
 		    String[] user4 = {"/red", "/orange", "/amber", "/green", "/blue", "/purple", "/white", "/lavender", "/HotRodPink", "/BritishRacingGreen"};
 		    String[] user5 = {"a"};
 
+		    ClickstreamData_Zume_Interview_redo c=new ClickstreamData_Zume_Interview_redo();
+		    c.findContiguousHistory(user0, user1);
+		    c.findContiguousHistory(user1, user2);
+		    c.findContiguousHistory(user2, user0);
+		    c.findContiguousHistory(user5, user2);
+		    c.findContiguousHistory(user3, user4);
+		    
+		   // below scenaro is tricky because amber match later and user3 string. We have to come back again.
+		  //  String[] user4 = {"/red", "/orange", "/amber", "/green", "/blue", "/purple", "/white", "/lavender", "/HotRodPink", "/BritishRacingGreen"};
+		   // String[] user3 = {"/red", "/orange", "/yellow", "/green", "/blue", "/purple", "/white", "/amber", "/HotRodPink", "/BritishRacingGreen"};
+		    
+		    c.findContiguousHistory(user4, user3);
+		    
+		    
+		    
+		    
 	}
 	
 	
-	public void findContiguousHistory(String user1, String user2)
+	public void findContiguousHistory(String[] user1Arr, String[] user2Arr)
 	{
+		if(user1Arr.length==0 || user2Arr.length==0)
+		{
+			System.out.println("Nothing common");
+		}
 		
+		List<String> result=new ArrayList<String>();
+				
 		
-	}
+		 List<String> resultSoFar=new ArrayList<String>();
+		 int i=0;int count=0;
+		 int j=0;
+		 boolean foundAtleatone=false;
+		 
+		 // loop for going thru both the string and move pointer one by one. Start from first user string and keep moving both the pointer until match
+		 // As soon as not match then check the so far length if it is greater then existing result and this will be new result
+		 // I have to take care special condition if some thing match in later part of string 2 so i was increamenting by two time so skipping one value
+		 //introduced new flage to see if any value found later because I already incremented to check the next value. It is not required i to increment again
+		 
+		 
+		 while( i<user1Arr.length && j<user2Arr.length)
+		 {		
+				 if(user1Arr[i].equalsIgnoreCase(user2Arr[j]))
+					{
+						resultSoFar.add(user1Arr[i]);
+						foundAtleatone=true;
+						 i++;
+						 j++;		
+						continue;
+					}
+					 else
+					 {
+						
+						 if(resultSoFar.size() > result.size())
+						 {
+							// System.out.println("resultSoFar " + resultSoFar.toString());
+							 //System.out.println("result " + result.toString());
+							 result.clear();
+							 result.addAll(resultSoFar);
+							 resultSoFar.clear();
+							 count=j;							
+							 foundAtleatone=false;
+						 }
+						 else
+						 {
+							 // clearing result list if length is smaller because it could be possible to have lesser match 
+							 // if not cleared then it will be added to final result
+							 
+							 resultSoFar.clear();
+						 }
+						 j++;
+						 
+						 if(j == user2Arr.length)
+						 {
+							 if(!foundAtleatone)
+								 i++;
+							 j=count;
+							 foundAtleatone=false;
+						 }
+						 
+					 }
+			 }
+			 
+		 if(resultSoFar.size() > result.size())
+		 {		
+			 result.clear();
+			 result.addAll(resultSoFar);
+			 resultSoFar.clear();
+		 }
+			 
+		 System.out.println("Final result " + result.toString());
+		
+	}	 
 
 }
