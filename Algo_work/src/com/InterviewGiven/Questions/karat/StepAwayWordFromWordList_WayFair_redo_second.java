93f7b5a971de3fa4be1e4396d571e310d840f43e
rajataggarwal1 <rajataggarwal2@gmail.com>
2019-09-28 21:26:47 -0700

    Completed program

--- Algo_work/src/com/InterviewGiven/Questions/karat/StepAwayWordFromWordList_WayFair_redo_second.java
+++ Algo_work/src/com/InterviewGiven/Questions/karat/StepAwayWordFromWordList_WayFair_redo_second.java
@@ -1,5 +1,14 @@
 package com.InterviewGiven.Questions.karat;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.URL;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
 /*
 https://public.karat.io/content/q093/wordlist.txt
 
@@ -25,19 +34,93 @@ index['ERA'] = ['REAL', 'RARE', 'AREA', 'READ', 'RATE', 'BEAR', 'NEAR', 'RACE', 
 index['JOY'] = []
 
 // Approach
-int count[]= new int[26];
+Create Map of the input string with count
+Read the file line by line and check string with is first element of two part string with length +1.
+If length is greater then +1(Because we are checking one step away string) then skip. else check whether 
+all character exist in file string. If yes and hashmap size is 0 then add into return string 
 
-for( to the stringlength)
-count[charAt(i)  - 'a']
 map(key as char, val as count)
 
 */
-
+// We need to reuse code of FindTopWordFromURL_WayFair_redo_first
 public class StepAwayWordFromWordList_WayFair_redo_second {
 
-	public static void main(String[] args) {
+	public static void main(String[] args) throws IOException {
 		// TODO Auto-generated method stub
-
+		StepAwayWordFromWordList_WayFair_redo_second f=new StepAwayWordFromWordList_WayFair_redo_second();
+		
+		List<String> result=f.findExactOneString("ERA");
+		System.out.println("ERA " + result);
+		
+		System.out.println("JOY " + f.findExactOneString("JOY"));
+		System.out.println("RAJA " + f.findExactOneString("RAJA"));
+	}
+	
+	public List<String> findExactOneString(String word) throws IOException
+	{
+		// Map the word in HasMap with its character count
+		Map<Character, Integer> charMap=new HashMap<Character,Integer>();
+		
+		// building map
+		for(int i=0; i< word.length(); i++)
+		{
+			charMap.put(word.charAt(i), charMap.getOrDefault( word.charAt(i), 0)+1 );			
+			
+		}
+		
+		List<String> result=new ArrayList<String>();
+		// reading file url
+		URL fileUrl=new URL("https://public.karat.io/content/q093/wordlist.txt");
+		BufferedReader bf= new BufferedReader(new InputStreamReader(fileUrl.openStream()));
+		String line;
+		
+			// reusing code of find top 1000 words logic 				
+				// going thru file line by line    
+			    while((line = bf.readLine()) !=null)
+			    {
+			    	String temp[]= line.split("\\t");
+			    	
+			    	// if size greater then continue
+			    	if(!(temp[0].length() == word.length()+1 ))
+			    	{
+			    		continue;
+			    	}
+			    	
+			    	// creating temp map to check with each upcoming string
+			    	Map<Character, Integer> tempMap=new HashMap<Character,Integer>();
+			    	tempMap.putAll(charMap); 
+			    	
+			    	for(int i=0; i< temp[0].length(); i++)
+					{
+			    		// checking in map and remove and decreasing the count
+			    		if(tempMap.containsKey(temp[0].charAt(i)))
+			    		{
+			    			int charCount=tempMap.get(temp[0].charAt(i));
+			    			charCount--;
+			    			if(charCount == 0)
+			    			{
+			    				tempMap.remove(temp[0].charAt(i));
+			    			}
+			    			else
+			    			{
+			    				tempMap.put(temp[0].charAt(i), charCount);
+			    			}
+			    			
+			    		}
+					}
+			    	
+			    	// building result
+			    	if(tempMap.size() == 0)
+			    	{
+			    		result.add(temp[0]);
+			    	}
+			    	
+			    }
+		
+				bf.close();
+			
+		return result;
+		    
 	}
 
 }
